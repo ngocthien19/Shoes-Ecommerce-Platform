3a18d195b6908d35c0d0607f19e37d3b34f9943d
Ngochuu2212 <phamngochuu3101@gmail.com>
2026-05-20 20:07:14 +0700

    complete API review-product-store

--- API_Shoes_Managemnt/src/routes/user/productRoute.js
+++ API_Shoes_Managemnt/src/routes/user/productRoute.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import { productController } from '~/controllers/user/productController'
+import { reviewController } from '~/controllers/user/reviewController'
 
 const router = express.Router()
 
 router.get('/homepage-products', productController.getHomepageProducts)
+router.get('/:slug/reviews', reviewController.getProductReviews)
 
 router.get('/search-filter', productController.searchAndFilterProducts)
 
