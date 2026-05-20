a6ad4bbbbe8a88c0bd39a3f221e7b4f2d46e04e7
ngocthien19 <caongocthien1902@gmail.com>
2026-05-20 20:33:04 +0700

    complete API cancel-withdraw-and-favorite

--- API_Shoes_Managemnt/src/routes/user/productRoute.js
+++ API_Shoes_Managemnt/src/routes/user/productRoute.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import { productController } from '~/controllers/user/productController'
 import { reviewController } from '~/controllers/user/reviewController'
+import { favoriteController } from '~/controllers/user/favoriteController'
+import { authGuard } from '~/middlewares/authGuard'
 
 const router = express.Router()
 
@@ -10,5 +12,9 @@ router.get('/:slug/reviews', reviewController.getProductReviews)
 router.get('/search-filter', productController.searchAndFilterProducts)
 
 router.get('/detail/:slug', productController.getProductDetail)
+
+router.post('/toggle', authGuard.isAuthorized, favoriteController.toggleFavorite)
+
+router.get('/', authGuard.isAuthorized, favoriteController.getFavoriteProducts)
 
 export const productRouter = router
---
+++ API_Shoes_Managemnt/src/services/user/favoriteService.js
@@ -0,0 +1,24 @@
+import { favoriteModel } from '~/models/product/favoriteModel'
+
+// Xử lý logic bật tắt Trái tim độc lập
+const toggleFavorite = async (userId, productId) => {
+  const isFavorite = await favoriteModel.checkFavoriteExist(userId, productId)
+
+  if (isFavorite) {
+    await favoriteModel.removeFavorite(userId, productId)
+    return { isFavorite: false, message: 'Đã xóa sản phẩm khỏi danh sách yêu thích.' }
+  } else {
+    await favoriteModel.addFavorite(userId, productId)
+    return { isFavorite: true, message: 'Đã thêm sản phẩm vào danh sách yêu thích thành công.' }
+  }
+}
+
+// Lấy danh sách Wishlist
+const getFavoriteProducts = async (userId) => {
+  return await favoriteModel.getFavoriteProductsByUserId(userId)
+}
+
+export const favoriteService = {
+  toggleFavorite,
+  getFavoriteProducts
+}
