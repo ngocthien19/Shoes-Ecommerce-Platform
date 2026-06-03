ddd0479062dd78b4a17a8764f97499be87630da1
ngocthien19 <caongocthien1902@gmail.com>
2026-06-04 03:36:51 +0700

    insert API get category for user

--- API_Shoes_Management/src/models/user/category/categoryModel.js
+++ API_Shoes_Management/src/models/user/category/categoryModel.js
@@ -1,7 +1,12 @@
 import pool from '~/config/db'
 
 const getAllCategories = async () => {
-  const query = 'SELECT id, name, slug, description, image FROM categories WHERE is_active = TRUE'
+  const query = `
+    SELECT id, parent_id, name, slug, description, image
+    FROM categories
+    WHERE is_active = TRUE
+    ORDER BY parent_id ASC, id ASC
+  `
   const [rows] = await pool.execute(query)
   return rows
 }
