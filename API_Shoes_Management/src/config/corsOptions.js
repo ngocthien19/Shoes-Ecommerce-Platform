53ba500b0dac871e3c62094b5508021ba5a03149
ngocthien19 <caongocthien1902@gmail.com>
2026-06-21 21:31:07 +0700

    feat: cors limit url

--- API_Shoes_Management/src/config/corsOptions.js
+++ API_Shoes_Management/src/config/corsOptions.js
@@ -1,11 +1,19 @@
 export const corsOptions = {
   origin: function (origin, callback) {
-    return callback(null, true)
+    // Danh sách các origin được phép
+    const allowedOrigins = [
+      'http://localhost:5173',
+      'https://shoes-ecommerce-platform.vercel.app'
+      // Thêm các domain khác nếu cần
+    ]
+
+    // Nếu không có origin (ví dụ: Postman, curl) hoặc origin được phép
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true)
+    } else {
+      return callback(new Error('Not allowed by CORS'), false)
+    }
   },
-
-  // Some legacy browsers (IE11, various SmartTVs) choke on 204
-  optionsSuccessStatus: 200,
-
-  // CORS sẽ cho phép nhận cookies từ request
-  credentials: true
+  credentials: true,
+  optionsSuccessStatus: 200
 }
